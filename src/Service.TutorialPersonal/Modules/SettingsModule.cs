eb08468781fdf40fd1aa668a0693f6e378ba4549
dmitriy.b <dmitriy.b@smplt.net>
2022-01-04 11:23:27 +0500

    Fix

--- src/Service.TutorialPersonal/Modules/SettingsModule.cs
+++ src/Service.TutorialPersonal/Modules/SettingsModule.cs
@@ -1,16 +1,16 @@
 ﻿using Autofac;
-using Service.Core.Domain.Models;
+using Service.Core.Domain;
 using Service.TutorialPersonal.Services;
 
 namespace Service.TutorialPersonal.Modules
 {
-    public class SettingsModule : Module
-    {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterInstance(Program.Settings).AsSelf().SingleInstance();
-	        builder.RegisterType<TutorialHelperService>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterType<ISystemClock>().AsImplementedInterfaces().SingleInstance();
-        }
-    }
+	public class SettingsModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterInstance(Program.Settings).AsSelf().SingleInstance();
+			builder.RegisterType<TutorialHelperService>().AsImplementedInterfaces().SingleInstance();
+			builder.RegisterType<SystemClock>().AsImplementedInterfaces().SingleInstance();
+		}
+	}
 }
